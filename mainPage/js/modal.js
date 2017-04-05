c8515c9c69f737881001130fa677693d2c5578a2
ym <crsmym88@gmail.com>
2017-04-05 16:06:39 +0900

    first commit

---
+++ mainPage/js/modal.js
@@ -0,0 +1,23 @@
+
+var modal_btn = document.querySelector('.modal_btn')
+var modal_window = document.querySelector('.modal_dim');
+var modal_close_btn = document.querySelector('.modal_close_btn');
+
+modal_btn.onclick = openModalWindow;
+modal_close_btn.onclick = closeModalWindow;
+ //지역분류 카테고리창 열기
+function openModalWindow() {
+  console.log('clicked open modal');
+  modal_window.style.display = 'block';
+}
+// 지역분류 카테고리창 닫기
+function closeModalWindow() {
+  console.log('clicked close modal');
+  modal_window.style.display = 'none';
+}
+
+var selected_region = document.getElementsByName('li').length;
+
+for( var i = 0; i < selected_region; i++ ) {
+  console.log(selected_region);
+}
